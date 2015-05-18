75cb61c2952c952c74efd4bee9ad5967f750e3bf
Zach Cox <zach.cox@banno.com>
2015-05-18 16:36:37 -0500

    hadoop 2.0.0-mr1-cdh4.4.0

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 mvn clean package install -DskipTests=true -Dmaven.test.skip=true
+
+cp services/target/druid-*-bin.tar.gz ../druid-docker/base
--- change-version.sh
+++ change-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -name pom.xml -print0 | xargs -0 sed -i '' 's|<version>0.7.1.1</version>|<version>0.7.1.1-hadoop-2.0.0-cdh4.4.0</version>|g'
+find . -type f -name pom.xml -print0 | xargs -0 sed -i '' 's|<version>0.7.1.1-hadoop-2.0.0-cdh4.4.0</version>|<version>0.7.1.1-hadoop-2.0.0-mr1-cdh4.4.0</version>|g'
